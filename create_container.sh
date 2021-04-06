2ac7772cb6f6ed5fe6df9388655dc6eef602e229
Sthope <6695214+Sthopeless@users.noreply.github.com>
2021-04-06 21:17:16 +0200

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -65,7 +65,7 @@ TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
 # Download setup script
-wget -qL https://github.com/Sthopeless/proxmox_docker_mqtt/raw/main/setup.sh
+wget -qL https://github.com/Sthopeless/proxmox_d3c0n5/raw/main/setup.sh
 
 # Detect modules and automatically load at boot
 load_module aufs
