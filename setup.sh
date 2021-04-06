10f25e29abdfff203627f59ac0cc0f2ce193406b
Sthope <6695214+Sthopeless@users.noreply.github.com>
2021-04-06 21:03:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,43 +35,10 @@ msg "Updating container OS..."
 apt-get update >/dev/null
 apt-get -qqy upgrade &>/dev/null
 
-# deconz step 1
-msg "Deconz step1..."
+# Install prerequisites
+msg "Installing prerequisites..."
 apt-get -qqy install \
-    curl \
-    kmod \
-    libcap2-bin \
-    libqt5core5a \
-    libqt5gui5 \
-    libqt5network5 \
-    libqt5serialport5 \
-    libqt5sql5 \
-    libqt5websockets5 \
-    libqt5widgets5 \
-    lsof \
-    sqlite3 \
-    tigervnc-standalone-server \
-    tigervnc-common \
-    wmii \
-    xfonts-base \
-    xfonts-scalable &>/dev/null
-
-# deconz step 2
-msg "Deconz step2..."
-apt-get clean  >/dev/null
-rm -rf /var/lib/apt/lists/* >/dev/null
-apt-get update >/dev/null
-apt-get -qqy install binutils 
-apt-get clean >/dev/null 
-rm -rf /var/lib/apt/lists/* >/dev/null 
-strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5 >/dev/null
-
-# deconz step 3
-msg "Deconz step3..."
-FOLDER_TMPDECONZ='/deconz_tmp'
-mkdir -p $(dirname $FOLDER_TMPDECONZ)
-wget -qL http://deconz.dresden-elektronik.de/ubuntu/stable/deconz-2.10.04-qt5.deb -O /deconz_tmp/deconz.deb
-# wget -qL https://github.com/Sthopeless/proxmox_d3c0n5/raw/main/setup.sh
+    curl &>/dev/null
 
 # Customize container
 msg "Customizing container..."
